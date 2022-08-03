451efa9b83cb4e6489ed22264290b2c1627b4347
J. Ricardo Silva <joao.ricardo.ifal@gmail.com>
2022-08-03 18:23:52 -0300

    add Images movie-categories

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
 import Row from "./components/Row";
 import categories from "./api";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
       {/* Navbar */}
       {/* Destaques */}
-      {/* Em alta */}
+      <Banner />
       {categories.map((category, index) => {
         return (
           <Row
             key={category.name}
             title={category.title}
             path={category.path}
+            isLarge={category.isLarge}
           />
         );
       })}
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { getMovies } from "../api";
+import "./Row.css";
 
-function Row({ title, path }) {
+const imageHost = "https://image.tmdb.org/t/p/original/";
+function Row({ title, path, isLarge }) {
   const [movies, setMovies] = React.useState([]);
 
   const fetchMovies = async (_path) => {
@@ -18,7 +20,25 @@ function Row({ title, path }) {
     fetchMovies(path);
   }, [path]);
 
-  return <div>Row</div>;
+  return (
+    <div className="row-container">
+      <h2 className="row-header">{title}</h2>
+      <div className="row-cards">
+        {movies?.map((movie) => {
+          return (
+            <img
+              className={`movie-card ${isLarge && "movie-card-large"}`}
+              key={movie.id}
+              src={`${imageHost}${
+                isLarge ? movie.backdrop_path : movie.poster_path
+              }`}
+              alt={movie.name}
+            ></img>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
